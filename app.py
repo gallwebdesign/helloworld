3c88673f81801cd91f58b3890644a494203c55d1
gallwebdesign <gallwebdesign@gmail.com>
2023-10-08 21:39:13 +0200

    Changed Case Again

--- app.py
+++ app.py
@@ -4,29 +4,26 @@ import pandas as pd
 import streamlit as st
 
 
-st.header('st-write')
+st.header("st-write")
 
-st.write('Hello, *World!* :sunglasses:')
+st.write("Hello, *World!* :sunglasses:")
 
-st.write('1234')
+st.write("1234")
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
+df = pd.DataFrame({"First Column": [1, 2, 3, 4], "Second Column": [10, 20, 30, 40]})
 st.write(df)
 
 
-st.write('Below is a DataFrame:', df, 'Above is a DataFrame.')
+st.write("Below is a DataFrame:", df, "Above is a DataFrame.")
 
 
 df2 = pd.DataFrame(
     np.random.randn(200, 3),
-    columns=['a', 'b', 'c'],)
-c = alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
+    columns=["a", "b", "c"],
+)
+c = (
+    alt.Chart(df2)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
 )
 st.write(c)
-
-
-
