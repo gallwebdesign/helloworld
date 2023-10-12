3c2cd5b52b6f9e41c2caca9d3fe1ebec1ab9aa5d
gallwebdesign <gallwebdesign@gmail.com>
2023-10-12 13:11:55 +0200

    Diusplay Correction

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ hide_st_style = """
     <style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
-    .viewerBadge_container__r5tak {display: hidden;}
+    .viewerBadge_container__r5tak {display: none;}
     </style>
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
