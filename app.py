0c3109a8fa2bddeec932c9a64ac7bc94bbce063e
gallwebdesign <gallwebdesign@gmail.com>
2023-10-12 11:55:53 +0200

    Test Visibiluity

--- app.py
+++ app.py
@@ -3,14 +3,6 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-hide_st_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    </style>
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
 
 st.header("st-write")
 
